2384dcf7eb4089d4b4ba617a63201157137233c1
KlinJava <alarmpinglin@gmail.com>
2024-02-20 16:51:35 +0800

    Java and Kotlin variable,data type

--- src/main/java/com/hank/Hello.java
+++ src/main/java/com/hank/Hello.java
@@ -1,2 +1,22 @@
-package com.hank;public class Hello {
+package com.hank;
+
+public class Hello {
+//    main方法產生：手動打全名、指令psvm+Tab 或 main+Tab 產生
+    public static void main(String[] args) {
+//        System.out.println("Hello!");
+        Person p = new Person();
+        p.hello();
+        int age = 19;   // int=32bits,short=16bits,long=64bits
+        Integer age2 = 19;  //基本資料型態，都有對應的參照資料型態物件，也稱包裝類別
+        System.out.println(age2.intValue());
+        char c = '我';   // char=16bits
+        Character c2 = 'A';
+        byte b = 120;   // byte=8bits
+        float weight = 66.5f;   // float=32bits,double=64bits
+        float height = 1.7f;
+        boolean adult = true;   // boolean=1bit
+        boolean enroll = false;
+        String name = "Tom";
+
+    }
 }
--- src/main/kotlin/com/hank/Hello.kt
+++ src/main/kotlin/com/hank/Hello.kt
@@ -2,5 +2,26 @@ package com.hank
 
 //main方法產生：指令main+Tab
 fun main() {
-    println("Hello Kotlin!")   //可使用sout+Tab產生
+//    println("Hello Kotlin!")   //可使用sout+Tab產生
+//    Human().hello()
+    val h = Human()
+    h.hello()
+    /* 特色
+    Kotlin沒有基本資料型態，只有物件的型態，如 Int，也稱「資料型態自動推斷」
+    分號，可有可無
+    */
+    var age : Int = 19
+    age = 20
+    var weight = 66.5f
+    val name : String
+    name = "Hank"
+
+
+}
+
+class Human {
+    fun hello() {   //不回傳任何資料，void不用寫
+        println("Hello kotlin")
+    }
+
 }

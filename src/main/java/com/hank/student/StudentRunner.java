d5782a8278da3e8694e27a92d300ad266c5fdc33
KlinJava <alarmpinglin@gmail.com>
2024-02-21 16:34:56 +0800

    第3-5_實務整合練習

--- src/main/java/com/hank/student/StudentRunner.java
+++ src/main/java/com/hank/student/StudentRunner.java
@@ -2,6 +2,7 @@ package com.hank.student;
 
 public class StudentRunner {
     public static void main(String[] args) {
-
+        Student stu = new Student("Hank",60,77);
+        stu.print();
     }
 }
---
+++ src/main/kotlin/com/hank/student/Student.kt
@@ -0,0 +1,14 @@
+package com.hank.kotlin
+
+fun main() {
+    val stu = Student("Jack",66,88)
+    stu.print()
+}
+
+class Student(var name:String,var english:Int,var math:Int) {
+    fun print(){
+        println(name + "\t" + english + "\t" + math +
+                "\t" + (english+math)/2)
+    }
+
+}
